8986ca423f8a787dc65d5ae233885519cb51ca57
EvanEmpyrean <39557676+EvanEmpyrean@users.noreply.github.com>
2023-10-24 22:13:15 +0200

    Initial Commit

---
+++ Centra/CentraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentraApp.swift
+//  Centra
+//
+//  Created by Evan Triadafillou on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CentraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
